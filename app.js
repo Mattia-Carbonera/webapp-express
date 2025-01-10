b1008bb1c376e11722f71afe4fbdcf1d975d811e
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-01-10 15:39:17 +0100

    check connection app and DB

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 // * INIZIALIZZO EXPRESS
 const express = require("express");
 const app = express();
+
+const host = "localhost";
 const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+
+// * COLLEGO IL DB
+const connection = require("./data/conn");
 
 // * ROTTE
 app.get("/", (req, res) => {
-  res.json({
-    status: "express OK",
+  const sqlMovie = "SELECT * FROM movies.movies";
+
+  connection.query(sqlMovie, (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    res.json(results);
   });
+  //   res.json({
+  //     status: "express OK",
+  //   });
 });
 
 // * METTO L'APP IN ASCOLTO
-app.listen(port, () => {
-  console.log("app in ascolto nella porta 3000");
+app.listen(3000, () => {
+  console.log(`app in ascolto a http://${host}:${port}`);
 });
