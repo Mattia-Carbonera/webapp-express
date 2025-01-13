84659160720457fa198dfe7420df7370cc5368c1
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-01-13 15:43:05 +0100

    add CORS

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 // * INIZIALIZZO EXPRESS
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 // * IMPORTO LE VARIABILI D'AMBIENTE
 const app_port = process.env.APP_PORT;
 const app_host = process.env.APP_HOST;
 
+// * CORS CONFIG
+var corsOption = {
+  origin: process.env.URL_FRONTEND_APP,
+  optionSuccessStatus: 200,
+};
+
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cors(corsOption));
 
 // * RICHIEDO I MIDDLEWARE
 const errorHandler = require("./middleware/errorHandler");
